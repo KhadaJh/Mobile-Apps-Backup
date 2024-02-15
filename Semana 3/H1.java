b6226ca3b23c0664a45083f7a27d036abb05b996
Peri <erickcaicedo0205@gmail.com>
2024-02-15 10:37:50 -0500

    Corrigiendo organizacion de archivos y sus contenidos

--- Semana 3/H1.java
+++ Semana 3/H1.java
@@ -1,38 +1,26 @@
-// 3^5 = 3 * 3^4
-// 3^4 = 3 * 3^3
-// 3^3 = 3 * 3^2 
-// 3^2 = 3 * 3 
-// 3^1 = 3 * 1
-
-//exp(n, 3-1) * n = exp (n, 2) * n
-//exp(n, 2-1) * n = exp (n, 1) * n
-//exp(n, 1-1) * n = exp (n, 0) * n
-//el if reemplaza el valor de exp (n,0), lo que significa que exp (n,0) = 1 * n
-
 import java.util.Scanner;
 
-public class H1 {
+public class PotenciaRecursiva {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el numero: ");
+        int num = sc.nextInt();
+        System.out.print("Introduce el exponente: ");
+        int exp = sc.nextInt();
 
-	public static class Exp {
-    		static int exp(int n, int x) {
-        		if (x == 0) {
-            			return 1;
-        		} else {
-            			return exp(n, x - 1) * n ;
-        		}
-    		}
-	}
+        int resultado = calcularPotencia(num, exp);
+        System.out.println("La potencia de " + num + " elevado a " + exp + " es: " + resultado);
+    }
 
-	public static void main(String[] args) {
-
-	        Scanner scanner = new Scanner(System.in);
-		
-	        System.out.print("Por favor, ingrese la base del numero que desee potenciar: ");
-        	int n = scanner.nextInt();
-
-	        System.out.print("Ahora ingrese un numero entero que quiera como exponente: ");
-			int x = scanner.nextInt();
-		
-       		System.out.println("El producto de " + n + " elevado a la " + x + " es: " + Exp.exp(n,x));
-	}
+    public static int calcularPotencia(int base, int exp) {
+        if (exp == 0) {
+            return 1;
+        } else if (exp > 0) {
+            
+            return base * calcularPotencia(base, exp - 1);
+        } else {
+            
+            return 1 / base * calcularPotencia(base, exp + 1);
+        }
+    }
 }
